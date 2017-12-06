2c277a7d1b0d1ea8e56abd36f4d0a5d6cf57462a
Xie Bing <xie.bing@vpclub.cn>
2017-12-06 16:13:14 +0800

    add all

---
+++ src/main/java/testj/HelloJenkins.java
@@ -0,0 +1,12 @@
+package testj;
+
+/**
+ * @author xiebing
+ * @create 2017-12-06 13:58
+ * @desc
+ **/
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins! another");
+    }
+}
